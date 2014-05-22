fd10d10cef794e0cc4c0da7c4c0e1faf04545ecf
Carlos <carlos10005@yahoo.com>
2014-05-22 13:56:36 -0500

    primer commit

---
+++ Assets/Test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Test : MonoBehaviour
+{
+    public List<string> enemigos;
+    private List<string> amigos = new List<string>();
+
+    private string enemigo = "";
+
+    void Start()
+    {
+        enemigos.Add("Licantropo");
+
+        Debug.Log(enemigo);
+    }
+}
